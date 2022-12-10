8a492d267c531e921b5a302dc754251a4164dd24
Amir <amirbhai432@gmail.com>
2022-12-10 12:49:32 +0530

    testing Purpose

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
-
 function App() {
   return (
     <div className="App">
-      <h1>is this my portFolio
-        
-      </h1>
-     
+      <h1>is this my portFolio</h1>
+      <h2>this line is testing for git</h2>
     </div>
   );
 }
